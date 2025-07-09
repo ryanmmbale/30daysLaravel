6e533c1c91c932b4229e6de83fe8252b69aef92c
ryanmmbale <mmbaleryan@gmail.com>
2025-07-10 01:17:10 +0300

    Tailwind UI

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -6,13 +6,74 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-<nav>
-    <x-nav href="/">Home</x-nav>
-    <x-nav href="/contact">Contact</x-nav>
-    <x-nav href="/about">About</x-nav>
-</nav>
-  {{$slot}}
+    <header class="bg-white">
+        <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+            <div class="flex lg:flex-1">
+                <a href="#" class="-m-1.5 p-1.5">
+                    <span class="sr-only">Your Company</span>
+                    <img class="h-16 w-auto" src="{{ asset('build/assets/ryanLogoBlack.png') }}" alt=""/>
+                </a>
+            </div>
+            <div class="flex lg:hidden">
+                <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+                </button>
+            </div>
+            <div class="hidden lg:flex lg:gap-x-12">
+
+                <a href="/" class="text-sm/6 font-semibold text-gray-900">Home</a>
+                <a href="/about" class="text-sm/6 font-semibold text-gray-900">About</a>
+                <a href="/contact" class="text-sm/6 font-semibold text-gray-900">Contact</a>
+            </div>
+            <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+                <a href="#" class="text-sm/6 font-semibold text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+            </div>
+        </nav>
+        <!-- Mobile menu, show/hide based on menu open state. -->
+        <div class="lg:hidden" role="dialog" aria-modal="true">
+            <!-- Background backdrop, show/hide based on slide-over state. -->
+            <div class="fixed inset-0 z-50"></div>
+            <div class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                <div class="flex items-center justify-between">
+                    <a href="#" class="-m-1.5 p-1.5">
+                        <span class="sr-only">Your Company</span>
+                        <img class="h-12 w-auto" src="{{ asset('build/assets/ryanLogoBlack.png') }}" alt="" />
+                    </a>
+                    <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
+                        <span class="sr-only">Close menu</span>
+                        <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
+                <div class="mt-6 flow-root">
+                    <div class="-my-6 divide-y divide-gray-500/10">
+                        <div class="space-y-2 py-6">
+
+                            <a href="/" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold
+                            text-gray-900 hover:bg-gray-50">Home</a>
+                            <a href="/about" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold
+                            text-gray-900 hover:bg-gray-50">About</a>
+                            <a href="/contact" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold
+                            text-gray-900 hover:bg-gray-50">Contact</a>
+                        </div>
+                        <div class="py-6">
+                            <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Log in</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <main>
+        {{ $slot }}
+    </main>
+
 </body>
 </html>
